5d4f6b67e456d8a4af905d445af4bdbdae620426
luojie <luojie2517@virtuos.com.cn>
2019-04-28 17:57:23 +0800

    add balls to scene

---
+++ Engine/gameplay/Ball.cpp
@@ -0,0 +1,43 @@
+﻿#include "Ball.h"
+
+
+
+Ball::Ball(WCHAR* texture)
+{
+	m_Texture = texture;
+	Initialize();
+}
+
+
+Ball::~Ball()
+{
+}
+
+
+bool Ball::Initialize()
+{
+	// 注册
+	Register();
+
+	bool result;
+
+	m_GraphicsComponent = new GraphicsComponentClass;
+
+	m_GraphicsComponent->Initialize("../Engine/data/ball.txt", m_Texture);//L"../Engine/data/Ball1.dds"
+
+	this->active = true;
+
+	m_PhysicsComponent = new PhysicsComponentClass;
+	// 速度静止
+	m_PhysicsComponent->Initialize();
+	m_PhysicsComponent->m_position = D3DXVECTOR3(1, 0, 0);
+	m_PhysicsComponent->m_collider = new SphereColliderClass(1);
+
+
+	return true;
+}
+
+bool Ball::Frame()
+{
+	return false;
+}
---
+++ Engine/gameplay/Ball.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "GameObjectClass.h"
+
+class Ball:public GameObjectClass
+{
+public:
+	Ball(WCHAR* texture);
+	~Ball();
+	virtual bool Initialize() override;
+	virtual bool Frame() override;
+	//继承了父类的shutdown 所以不用考虑组件的析构
+private:
+	WCHAR* m_Texture;
+};
+
