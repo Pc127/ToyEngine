6a84538251f25a3777a5e426c4c3411aa1a598e2
xieshiheng <xieshiheng2516@virtuos.com.cn>
2019-04-28 11:32:15 +0800

    gravity and impulse

---
+++ Engine/physics/ImpulseForceClass.cpp
@@ -0,0 +1,36 @@
+﻿#include "ImpulseForceClass.h"
+
+
+
+ImpulseForceClass::ImpulseForceClass(D3DXVECTOR3 impu)
+{
+	this->impulse= impu;
+}
+
+
+ImpulseForceClass::~ImpulseForceClass()
+{
+}
+
+bool ImpulseForceClass::Initialize()
+{
+	active = true;
+	return true;
+}
+
+bool ImpulseForceClass::Frame(PhysicsComponentClass *pc, float deltatime)
+{
+	// 利用重力更新速度
+	pc->m_velocity += impulse;
+
+	// 一帧之后 该冲量失效
+	active = false;
+		
+	return true;
+}
+
+bool ImpulseForceClass::IsActive()
+{
+	// 返回
+	return active;
+}
