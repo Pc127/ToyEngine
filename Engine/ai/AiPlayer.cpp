b7572aaf8ea6234c7b2f8941d0a4ceb3716457e9
xieshiheng <xieshiheng2516@virtuos.com.cn>
2019-05-10 16:02:13 +0800

    add ai

---
+++ Engine/ai/AiPlayer.cpp
@@ -0,0 +1,39 @@
+﻿#include "Table.h"
+
+
+
+Table::Table()
+{
+	Initialize();
+}
+
+
+Table::~Table()
+{
+}
+
+
+bool Table::Initialize()
+{
+	// 注册
+	Register();
+
+	bool result;
+	m_GraphicsComponent = new GraphicsComponentClass;
+
+	m_GraphicsComponent->Initialize("../Engine/data/Table.txt", L"../Engine/data/Table.dds");
+
+	this->active = true;
+
+	m_PhysicsComponent = new PhysicsComponentClass;
+	// 速度静止
+	m_PhysicsComponent->Initialize();
+	m_PhysicsComponent->m_position = D3DXVECTOR3(0, 0, 0);
+
+	return true;
+}
+
+bool Table::Frame()
+{
+	return false;
+}
